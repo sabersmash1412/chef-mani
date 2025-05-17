8c6857e806c37e2373e3d752a8fe96e7e34247e3
Annamalai Maniveerappan <121749563+sabersmash1412@users.noreply.github.com>
2025-05-17 22:31:13 +0800

    Header-and-input

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+    return (
+        <header>
+            <img src="/images/ChatGPT Image May 15, 2025, 06_10_21 PM.png" alt="Chef Icon" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+} 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default function Main() {
+    return (
+        <main>
+            <form className="add-ingredient-form">
+                <input 
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+        </main>
+    )
+}
