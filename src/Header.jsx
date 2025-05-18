4a254f3eae508f69ee9f0f3fbccd95e1e736677a
Annamalai Maniveerappan <121749563+sabersmash1412@users.noreply.github.com>
2025-05-18 17:44:43 +0800

    Form

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header>
             <img src="/images/ChatGPT Image May 15, 2025, 06_10_21 PM.png" alt="Chef Icon" />
-            <h1>Chef Claude</h1>
+            <h1>Chef Mani</h1>
         </header>
     )
 } 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,30 @@
+import React from "react"
+
 export default function Main() {
+
+    const [ingredient, setIngredient] = React.useState([])
+
+    function submit(formData) {
+        const newIngredient = formData.get("ingredient")
+        if (newIngredient.trim()) {
+            setIngredient(prev => [...prev, <li key={newIngredient}>{newIngredient}</li>])
+        }
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form action={submit} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredient}
+            </ul>
         </main>
     )
 }
